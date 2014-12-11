36e97914637c46142a5bdc628c20ea2b08c80262
kalo-glb <kalo.glb@gmail.com>
2014-12-12 01:45:00 +0200

    working without server and database but no timer

--- WebComunication.py
+++ WebComunication.py
@@ -1,65 +1,22 @@
 __author__ = 'kalo'
 import threading
-from urllib import *
-import websocket
 import time
 
 
 class WebComunication(threading.Thread):
-    def __init__(self, host, port, in_queue, out_queue, stop_event):
+    def __init__(self, in_queue, out_queue, stop_event):
         threading.Thread.__init__(self)
-        self.host = host
-        self.port = port
-        #self.init_server()
         self.in_queue = in_queue
         self.out_queue = out_queue
         self.stop_event = stop_event
-        self.wait_for_response = 0
-
-    def init_server(self):
-        (sid, hbtimeout, ctimeout) = self.handshake(self.host, self.port)
-        self.server = websocket.create_connection("ws://%s:%d/socket.io/1/websocket/%s" % (self.host, self.port, sid))
-        print(self.server.recv())
-        self.server.send("2::")
-        #self.server.send('5:1::{"name":"system_ready", "args":"system_ready"}')
-        #print(self.server.recv())
-        #print(self.server.recv())
-
-    def handshake(self, host, port):
-        u = urlopen("http://%s:%d/socket.io/1/" % (host, port))
-        if u.getcode() == 200:
-            response = u.readline()
-            (sid, hbtimeout, ctimeout, supported) = response.split(":")
-            supportedlist = supported.split(",")
-            if "websocket" in supportedlist:
-                return (sid, hbtimeout, ctimeout)
-            else:
-                raise TransportException()
-        else:
-            raise InvalidResponseException()
+        self.card_map_dict = {"6A003E858253": "obbo1", "67007BBBEB4C": "obbb2", "6A003E6BA09F": "oobo3"}
 
     def run(self):
         while not self.stop_event.is_set():
             if not self.in_queue.empty():
                 data = self.in_queue.get(block=False)
-                self.init_server()
                 print(str(data[1]))
-                self.server.send('5:1::{"name":"' + data[0] + '", "args":"' + str(data[1]) + '"}' )
                 if data[0] == "getPattern":
-                    self.wait_for_response = 2
-                elif data[0] == "end" or data[0] == "playerError":
-                    self.wait_for_response = 1
-                else:
-                    self.wait_for_response = 2
-            if 0 != self.wait_for_response:
-                self.wait_for_response -= 1
-                recieved_data = self.server.recv()
-                print("log: %s" % recieved_data)
-                if recieved_data[0] == '3':
-                    pattern = recieved_data.split(':')[-1]
-                    self.out_queue.put(pattern)
-
-                if self.wait_for_response == 0:
-                    self.server.close()
+                    self.out_queue.put(self.card_map_dict[data[1]])
 
             time.sleep(0.001)
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 __author__ = 'kalo'
 from SerialManager import SerialManager
 from Events import Events, EventTypes
-from  WebComunication import WebComunication
+from WebComunication import WebComunication
 import threading
 import Queue
 import time
-from urllib import *
-import websocket
 
 
 class Manager():
-    def __init__(self, host, port):
+    def __init__(self):
         self.stop_event = threading.Event()
         self.to_serial = Queue.Queue()
         self.from_serial = Queue.Queue()
@@ -19,7 +17,7 @@ class Manager():
         self.serial_manager = SerialManager("/dev/ttyACM0", 9600, "/dev/ttyUSB0", 9600,
                                             self.stop_event,
                                             self.to_serial, self.from_serial)
-        self.web_com = WebComunication(host, port, self.to_web, self.from_web, self.stop_event)
+        self.web_com = WebComunication(self.to_web, self.from_web, self.stop_event)
 
     def run(self):
         self.serial_manager.start()
@@ -29,12 +27,10 @@ class Manager():
                 data = ("error", "error")
                 event = self.from_serial.get(block=False)
                 if event["event_type"] == EventTypes.rfid_event:
-                    #print("debug: getPattern")
                     self.time_start = time.time()
                     data = ("getPattern", event["e"])
                 if event["event_type"] == EventTypes.mcu_event:
                     if event["e"] == Events.player_won:
-                        #print("debug: end")
                         data = ("end", time.time() - self.time_start)
                     elif event["e"] == Events.player_error:
                         data = ("playerError", time.time() - self.time_start)
@@ -48,6 +44,6 @@ class Manager():
             time.sleep(0.01)
 
 
-man = Manager("192.168.0.101", 8080)
+man = Manager()
 man.run()
 
